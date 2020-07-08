8103daefab4dbcaa06e56a6ae6a78b07f62dad14
blubbers122 <blubbers122@users.noreply.github.com>
2020-07-08 16:38:25 -0500

    first commit
    got an old project up and running using create-react-app.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import $ from "jquery";
 
 function App() {
   return (
@@ -23,4 +24,289 @@ function App() {
   );
 }
 
-export default App;
+export class BeatMaker extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      notePosition: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16],
+      instrumentBank: [
+        {
+          drum: "Crash1",
+          url: "https://freewavesamples.com/files/Crash-Cymbal-1.wav"
+        },
+        {
+          drum: "Crash2",
+          url: "https://freewavesamples.com/files/Crash-Cymbal-2.wav"
+        },
+        {
+          drum: "RideBell",
+          url: "https://freewavesamples.com/files/Roland-SC-88-Ride-Bell.wav"
+        },
+        {
+          drum: "Ride",
+          url: "https://freewavesamples.com/files/Ride-Cymbal-1.wav"
+        },
+        {
+          drum: "Tom1",
+          url: "https://freewavesamples.com/files/Hi-Tom-1.wav"
+        },
+        {
+          drum: "Tom2",
+          url: "https://freewavesamples.com/files/Floor-Tom-1.wav"
+        },
+        {
+          drum: "Tom3",
+          url: "https://freewavesamples.com/files/Floor-Tom-3.wav"
+        },
+        {
+          drum: "HHOpen",
+          url: "https://freewavesamples.com/files/Open-Hi-Hat-1.wav"
+        },
+        {
+          drum: "HHClosed",
+          url: "https://freewavesamples.com/files/Closed-Hi-Hat-1.wav"
+        },
+        {
+          drum: "HHPedal",
+          url: "https://freewavesamples.com/files/Pedal-Hi-Hat-1.wav"
+        },
+        {
+          drum: "Snare",
+          url: "https://freewavesamples.com/files/Roland-R-8-Fat-Snare.wav"
+        },
+        {
+          drum: "Kick",
+          url: "https://freewavesamples.com/files/Bass-Drum-1.wav"
+        }
+      ],
+      paused: true,
+      selectedNotes: [],
+      bpm: 100
+    }
+   this.handleClick = this.handleClick.bind(this);
+   this.handleControls = this.handleControls.bind(this);
+   this.prepareBeat = this.prepareBeat.bind(this);
+   this.playBeat = this.playBeat.bind(this);
+   this.stopBeat = this.stopBeat.bind(this);
+  }
+
+  handleClick(drum, url, pos) {
+    var currentNote = ("#" + drum + "-" + pos);
+    var selectedNotes = this.state.selectedNotes;
+    var myAudio = new Audio(url);
+    var velocity = $(currentNote).attr("velocity")
+    $("#reset").css("animation", "none");
+
+    console.log(velocity)
+
+    if (url == "clear note") {
+      $(currentNote).attr("velocity", 0);
+      $(currentNote).css("background", "none");
+      selectedNotes.splice(selectedNotes.indexOf(currentNote), 1)
+    }
+    else {
+      switch(velocity) {
+        case "0":
+          $(currentNote).attr("velocity", 4)
+          $(currentNote).css("background", "white")
+          myAudio.play()
+          selectedNotes.push(currentNote)
+          break;
+        case "1":
+          $(currentNote).attr("velocity", 0);
+          $(currentNote).css("background", "none");
+          selectedNotes.splice(selectedNotes.indexOf(currentNote), 1)
+          break;
+        case "2":
+          $(currentNote).attr("velocity", 1);
+          $(currentNote).css("background", "#444444");
+          myAudio.volume = .25;
+          myAudio.play()
+          break;
+        case "3":
+          $(currentNote).attr("velocity", 2);
+          $(currentNote).css("background", "#888888");
+          myAudio.volume = .50;
+          myAudio.play()
+          break;
+        case "4":
+          $(currentNote).attr("velocity", 3);
+          $(currentNote).css("background", "#BBBBBB");
+          myAudio.volume = .75;
+          myAudio.play()
+          break;
+      }
+    }
+  }
+
+  handleControls(control) {
+    if (control == "stop") {
+      this.stopBeat();
+      return (
+        this.setState({
+          paused: true
+        })
+      )
+    }
+    else if (control == "start" && this.state.selectedNotes.length > 0) {
+      this.prepareBeat();
+      return (
+        this.setState({
+          paused: false
+        })
+      )
+    }
+    else if (control == "reset") {//reset
+      $("#reset").css("animation", "spin 1s");
+      this.stopBeat();
+      this.state.selectedNotes.forEach(note => (
+        $(note).css("background", "black"),
+        $(note).attr("velocity", "0")
+      ))
+      return (
+        this.setState({
+          paused: true,
+          selectedNotes: [],
+          bpm: 100
+        })
+      )
+    }
+  }
+
+  changeInput(event) {
+    return (
+      this.setState({
+        bpm: event.target.value
+      })
+    )
+  }
+
+  prepareBeat() {
+
+
+
+    this.playBeat()
+  }
+
+  playBeat() {
+    var state = this.state;
+    var beat = [];// holds arrays containing
+    var index = -1;
+    for (let i = 1; i < 17; i++) {//repeats for each 16th note
+      var regex = new RegExp("-" + i.toString() + "$");//for searching for notes on current 16th note
+      var noteStack = 0;//items in 2nd array in beat
+
+      for (let j = 0; j < state.selectedNotes.length; j++) {//repeats for each selected note
+        var note = state.selectedNotes[j]//current selected note
+        if (note.match(regex)) {
+          if (noteStack != 0) {//if not first note in stack
+            beat[index].push(note);
+          }
+          else {//if first note in stack
+            beat.push([note]);
+            index++;
+          }
+          noteStack++//each match adds an array item
+        }
+      }
+    }
+
+    let count = 0;
+    let beatIndex = 0;
+
+    this.timer = setInterval(function () {
+      var countRegex = new RegExp("-" + count.toString() + "$");
+
+      if (beat[beatIndex][0].match(countRegex)) {
+        for (let x = 0; x < beat[beatIndex].length; x++) {
+          let chosenDrum = beat[beatIndex][x].replace("-" + count.toString(), "").replace("#", "")
+          var audio = new Audio(state.instrumentBank.find( ({ drum }) => drum === chosenDrum ).url)
+          audio.volume = $(beat[beatIndex][x]).attr("velocity") / 4
+          audio.play()
+        }
+        if (beatIndex != beat.length - 1) {
+          beatIndex++;
+        }
+      }
+      if (count == 16) {
+        count = 0;
+        beatIndex = 0;
+      }
+      console.log(count)
+      count++;
+    }, 15000 / state.bpm)//repeats each 16th note
+  }
+
+  stopBeat() {
+    clearInterval(this.timer)
+  }
+
+  render() {
+    return (
+      <div id="beat-maker-container">
+        <h1 id="title">Drum Beat Maker</h1>
+        <div id="beat-count-label" className="row">
+          {this.state.notePosition.map(note => (
+            <h6 id={"count" + note} className="count">{note}</h6>
+          ))}
+        </div>
+        <div id="labels-and-grid" className="row">
+          <div>
+            {this.state.instrumentBank.map(instrument => (
+              <h6 id="drum-label">{instrument.drum}</h6>
+            ))
+            }
+          </div>
+          <div id="composer-grid-box">
+            <div id="drum-row" className="row">
+              
+              {this.state.notePosition.map(pos => (//creates every row
+                <Instrument
+                  bank={this.state.instrumentBank}
+                  handleClick={(drum, url, pos) => this.handleClick(drum, url, pos)}
+                  pos={pos} />
+              ))}
+            </div>
+          </div>
+        </div>
+        <ControlPanel
+          paused={this.state.paused}
+          callback={(control) => this.handleControls(control)}
+          bpm={this.state.bpm}
+          input={this.changeInput.bind(this)}/>
+      </div>
+    )
+  }
+}
+
+const Instrument = props => {
+  return (
+    <div>
+      {props.bank.map(instrument => (//creates every column
+        <div
+          className="note"
+          id={instrument.drum + "-" + props.pos}
+          velocity={0}
+          onClick={() => props.handleClick(instrument.drum, instrument.url, props.pos)}
+          onMouseDown={(event) => event.button == 1 ? props.handleClick(instrument.drum, "clear note", props.pos) : ""}>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+const ControlPanel = props => {
+  return (
+    <div id="control-panel-container" className="row">
+      { props.paused &&
+        <i id="start_stop" onClick={() => props.callback("start")} className="fas fa-play hvr-grow"></i>
+      }
+      { !props.paused &&
+        <i id="start_stop" onClick={() => props.callback("stop")} className="fas fa-pause hvr-grow"></i>
+      }
+        <i id="reset" onClick={() => props.callback("reset")} className="fas fa-sync-alt hvr-grow"></i>
+      <input id="bpm" value={props.bpm} onChange={props.input} maxLength="3" />
+      <h6 id="bpm-label">bpm</h6>
+    </div>
+  )
+}
